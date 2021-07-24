04caadde3e904f83ea97d8f06edc104408cc02d0
felipe ls <feliph_ls@hotmail.com>
2021-07-24 08:52:54 -0500

    agregando validaciones al formulario de login

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -8,11 +8,37 @@ import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
 })
 export class LoginPage implements OnInit {
   loginForm: FormGroup;
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  validation_messages = [
+  {
+    email: [
+      {
+        type: 'required',
+        message: 'Este campo es requerido'
+      },
+      {
+        type: 'pattern',
+        message: 'Este no es un correo valido'
+      }
+    ],
+    password: [
+      {
+        type: 'required',
+        message: 'Este campo es requerido'
+      },
+      {
+        type: 'minLength',
+        message: 'Minimo 8 caracteres'
+      }
+    ]
+  }
+];
+
   constructor(private formBuider: FormBuilder) {
     this.loginForm = this.formBuider.group({
       email: new FormControl('', Validators.compose([
         Validators.required,
-        Validators.pattern('^[a-zA-Z0-9_+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]$')
+        Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')
       ])),
       password: new FormControl('', Validators.compose([
         Validators.required,
